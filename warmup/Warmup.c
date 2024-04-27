5e9a566f6862b1ea32a07629d08428c6348300d1
Francisco Molina <francisco.molina.antonio@gmail.com>
2024-04-27 19:19:43 +0100

    add README, CH

--- warmup/Warmup.c
+++ warmup/Warmup.c
@@ -1,47 +1,50 @@
 #include <stdio.h>
+#include <ctype.h> // Para usar la función isdigit
 
 int main() {
-    int ingreso;
+    char ingreso;
 
-    printf("ml' nob:\n");  
-    scanf("%d", &ingreso);
+    printf("ml' nob:\n");
+    scanf(" %c", &ingreso); // Usamos %c para leer un carácter
+    // Verificar si el ingreso es una letra
+    if (isalpha(ingreso)) {
+        printf("Neh mi'\n");
+    } else if (ingreso >= '0' && ingreso <= '8') {
+        // Si es un número entre 0 y 8, convertimos el carácter a entero
+        int numero = ingreso - '0';
 
-    switch (ingreso){
-        case 0:
-            printf("Qapla'\nnoH QapmeH wo' Qaw'lu'chugh yay chavbe'lu' 'ej wo' choqmeH may' DoHlu'chugh lujbe'lu'\n");
-            break;
-        case 1:
-            printf("Qapla'\nbortaS bIr jablu'DI' reH QaQqu' nay'.\n");
-            break;
-        case 2:
-            printf("Qapla'\nQu' buSHa'chugh SuvwI', batlhHa' vangchugh, qoj matlhHa'chugh, pagh ghaH SuvwI''e'.\n");
-            break;
-        case 3:
-            printf("Qapla'\nbISeH'eghlaH'be'chugh latlh Dara'laH'be'.\n");
-            break;
-        case 4:     
-            printf("Qapla'\nqaStaHvIS wa' ram loS SaD Hugh SIjlaH qetbogh loD.\n");
-            break;
-        case 5:     
-            printf("Qapla'\nSuvlu'taHvIS yapbe' HoS neH.\n");
-            break;
-        case 6:     
-            printf("Qapla'\nHa'DIbaH DaSop 'e' DaHechbe'chugh yIHoHQo'.\n"); 
-            break;
-        case 7:    
-            printf("Qapla'\nHeghlu'meH QaQ jajvam.\n");
-            break;
-        case 8:
-            printf("Qapla'\nleghlaHchu'be'chugh mIn lo'laHbe' taj jej.\n");
-            break;
+        switch (numero) {
+            case 0:
+                printf("Qapla'\nnoH QapmeH wo' Qaw'lu'chugh yay chavbe'lu' 'ej wo' choqmeH may' DoHlu'chugh lujbe'lu'\n");
+                break;
+            case 1:
+                printf("Qapla'\nbortaS bIr jablu'DI' reH QaQqu' nay'.\n");
+                break;
+            case 2:
+                printf("Qapla'\nQu' buSHa'chugh SuvwI', batlhHa' vangchugh, qoj matlhHa'chugh, pagh ghaH SuvwI''e'.\n");
+                break;
+            case 3:
+                printf("Qapla'\nbISeH'eghlaH'be'chugh latlh Dara'laH'be'.\n");
+                break;
+            case 4:
+                printf("Qapla'\nqaStaHvIS wa' ram loS SaD Hugh SIjlaH qetbogh loD.\n");
+                break;
+            case 5:
+                printf("Qapla'\nSuvlu'taHvIS yapbe' HoS neH.\n");
+                break;
+            case 6:
+                printf("Qapla'\nHa'DIbaH DaSop 'e' DaHechbe'chugh yIHoHQo'.\n");
+                break;
+            case 7:
+                printf("Qapla'\nHeghlu'meH QaQ jajvam.\n");
+                break;
+            case 8:
+                printf("Qapla'\nleghlaHchu'be'chugh mIn lo'laHbe' taj jej.\n");
+                break;
+        }
+    } else {
+        printf("Qih mi'\n");
     }
 
-    if (ingreso>8 || ingreso<0){
-        printf("Qih mi %d\n",ingreso);
-    }
-
-    
-return 0;
-
+    return 0;
 }
-
