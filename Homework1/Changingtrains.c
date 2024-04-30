3a0195817061b1baf0d4be7a0502858a12da0576
Francisco Molina <francisco.molina.antonio@gmail.com>
2024-04-30 02:04:50 +0100

    version. 1 (structre of printf)

---
+++ Homework1/Changingtrains.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+
+
+int main (){
+
+int Aarrivalh, Aarrivalm, Adepartureh ,Adeparturem;
+//int Barrival, Bdeparture;
+//int Carrival,  Cdeparture;
+
+
+printf("Train A arrival time:\n");
+scanf("%d:%d",&Aarrivalh,&Aarrivalm);
+printf("Train A departure time:\n");
+scanf("");
+
+printf("Train B arrival time:\n");
+scanf("");
+
+printf("Train B departure time:\n");
+scanf("");
+
+printf("Train C arrival time:\n");
+scanf("");
+
+printf("Train C departure time:\n");
+scanf("");
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
