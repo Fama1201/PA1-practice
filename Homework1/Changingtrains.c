6fb17c2c6b9e63e81885b179184617498b6715be
Francisco Molina <francisco.molina.antonio@gmail.com>
2024-07-20 22:15:49 +0100

    Prueba

--- Homework1/Changingtrains.c
+++ Homework1/Changingtrains.c
@@ -1,3 +1,4 @@
+//DEBER 1 PA1
 #include <stdio.h>
 
 int main (){
@@ -11,40 +12,36 @@ if(scanf("%d:%d",&Aarrivalh,&Aarrivalm)!=2|| Aarrivalh>24||Aarrivalh<0||Aarrival
     printf("Invalid input\n");
     return 0;
 }
-
 printf("Train A departure time:\n");
 if(scanf("%d:%d",&Adepartureh ,&Adeparturem)!=2 || Adepartureh>24 || Adepartureh<0 || Adeparturem>59 ||Adeparturem<0){ //Validacion de horario
     printf("Invalid input\n");
     return 0;
 }
-
 printf("Train B arrival time:\n");
 if(scanf("%d:%d",&Barrivalh,&Barrivalm)!=2|| Barrivalh>24 || Barrivalh<0|| Barrivalm>59 || Barrivalm< 0){ //Validacion de horario
     printf("Invalid input\n");
     return 0;
 }
-
 printf("Train B departure time:\n");
 if(scanf("%d:%d",&Bdepartureh,&Bdeparturem)!=2 || Bdepartureh>24 || Bdepartureh<0|| Bdeparturem>59||Bdeparturem<0){
     printf("Invalid input\n");
     return 0;
 }
-
 printf("Train C arrival time:\n");
 if(scanf("%d:%d",&Carrivalh,&Carrivalm)!=2 || Carrivalh>24 || Carrivalh<0 || Carrivalm>59 ||Carrivalm<0){
     printf("Invalid input\n");
     return 0;
 }
-
 printf("Train C departure time:\n");
 if(scanf("%d:%d",&Cdepartureh, &Cdeparturem)!=2 || Cdepartureh>24 ||Cdepartureh<0 ||Cdeparturem>59 || Cdeparturem<0){
-
     printf ("Invalid input\n");
     return 0;
 }
 
 
+// MENSAJE PARA TREN A
 
+if()
 
 
 
