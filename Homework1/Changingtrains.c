9a117162458cd462940503fe88965dcbff7a0552
Francisco Molina <francisco.molina.antonio@gmail.com>
2024-07-22 14:19:25 +0100

    Final version

--- Homework1/Changingtrains.c
+++ Homework1/Changingtrains.c
@@ -6,45 +6,45 @@ int main() {
     int Carrivalh, Carrivalm, Cdepartureh, Cdeparturem;
 
     // Train A times input and validation
-    printf("Train A arrival time (hh:mm):\n");
+    printf("Train A arrival time:\n");
     if (scanf("%d:%d", &Aarrivalh, &Aarrivalm) != 2 || Aarrivalh > 23 || Aarrivalh < 0 || Aarrivalm > 59 || Aarrivalm < 0) {
         printf("Invalid input.\n");
         return 0;
     }
 
-    printf("Train A departure time (hh:mm):\n");
+    printf("Train A departure time:\n");
     if (scanf("%d:%d", &Adepartureh, &Adeparturem) != 2 || Adepartureh > 23 || Adepartureh < 0 || Adeparturem > 59 || Adeparturem < 0) {
         printf("Invalid input.\n");
         return 0;
     }
 
     // Train B times input and validation
-    printf("Train B arrival time (hh:mm):\n");
+    printf("Train B arrival time:\n");
     if (scanf("%d:%d", &Barrivalh, &Barrivalm) != 2 || Barrivalh > 23 || Barrivalh < 0 || Barrivalm > 59 || Barrivalm < 0) {
         printf("Invalid input.\n");
         return 0;
     }
 
-    printf("Train B departure time (hh:mm):\n");
+    printf("Train B departure time:\n");
     if (scanf("%d:%d", &Bdepartureh, &Bdeparturem) != 2 || Bdepartureh > 23 || Bdepartureh < 0 || Bdeparturem > 59 || Bdeparturem < 0) {
         printf("Invalid input.\n");
         return 0;
     }
 
     // Train C times input and validation
-    printf("Train C arrival time (hh:mm):\n");
+    printf("Train C arrival time:\n");
     if (scanf("%d:%d", &Carrivalh, &Carrivalm) != 2 || Carrivalh > 23 || Carrivalh < 0 || Carrivalm > 59 || Carrivalm < 0) {
         printf("Invalid input.\n");
         return 0;
     }
 
-    printf("Train C departure time (hh:mm):\n");
+    printf("Train C departure time:\n");
     if (scanf("%d:%d", &Cdepartureh, &Cdeparturem) != 2 || Cdepartureh > 23 || Cdepartureh < 0 || Cdeparturem > 59 || Cdeparturem < 0) {
         printf("Invalid input.\n");
         return 0;
     }
 
-
+    // 0h to 24h control
     if(Aarrivalh==0){
         Aarrivalh=24;
     }
@@ -78,37 +78,37 @@ int main() {
     // Check transfers for Train A
     if (AarrivalTimeInMinutes + 5 <= BdepartureTimeInMinutes && AarrivalTimeInMinutes + 180 >= BarrivalTimeInMinutes
         && AarrivalTimeInMinutes + 5 <= CdepartureTimeInMinutes && AarrivalTimeInMinutes + 180 >= CarrivalTimeInMinutes) {
-        puts("Can change to both B and C from A");
+        puts("Can change to both B and C from A.");
     } else if (AarrivalTimeInMinutes + 5 <= BdepartureTimeInMinutes && AarrivalTimeInMinutes + 180 >= BarrivalTimeInMinutes) {
-        puts("Can change to B from A");
+        puts("Can change to B from A.");
     } else if (AarrivalTimeInMinutes + 5 <= CdepartureTimeInMinutes && AarrivalTimeInMinutes + 180 >= CarrivalTimeInMinutes) {
-        puts("Can change to C from A");
+        puts("Can change to C from A.");
     }else{
-        puts("No changes available from train A");
+        puts("No changes available from train A.");
         }
 
     // Check transfers for Train B
     if (BarrivalTimeInMinutes + 5 <= AdepartureTimeInMinutes && BarrivalTimeInMinutes + 180 >= AarrivalTimeInMinutes
         && BarrivalTimeInMinutes + 5 <= CdepartureTimeInMinutes && BarrivalTimeInMinutes + 180 >= CarrivalTimeInMinutes) {
-        puts("Can change to both A and C from B");
+        puts("Can change to both A and C from B.");
     } else if (BarrivalTimeInMinutes + 5 <= AdepartureTimeInMinutes && BarrivalTimeInMinutes + 180 >= AarrivalTimeInMinutes) {
-        puts("Can change to A from B");
+        puts("Can change to A from B.");
     } else if (BarrivalTimeInMinutes + 5 <= CdepartureTimeInMinutes && BarrivalTimeInMinutes + 180 >= CarrivalTimeInMinutes) {
-        puts("Can change to C from B");
+        puts("Can change to C from B.");
     }else{
-        puts("No changes available from train B");
+        puts("No changes available from train B.");
         }
 
     // Check transfers for Train C
     if (CarrivalTimeInMinutes + 5 <= AdepartureTimeInMinutes && CarrivalTimeInMinutes + 180 >= AarrivalTimeInMinutes
         && CarrivalTimeInMinutes + 5 <= BdepartureTimeInMinutes && CarrivalTimeInMinutes + 180 >= BarrivalTimeInMinutes) {
-        puts("Can change to both A and B from C");
+        puts("Can change to both A and B from C.");
     } else if (CarrivalTimeInMinutes + 5 <= AdepartureTimeInMinutes && CarrivalTimeInMinutes + 180 >= AarrivalTimeInMinutes) {
-        puts("Can change to A from C");
+        puts("Can change to A from C.");
     } else if (CarrivalTimeInMinutes + 5 <= BdepartureTimeInMinutes && CarrivalTimeInMinutes + 180 >= BarrivalTimeInMinutes) {
-        puts("Can change to B from C");
+        puts("Can change to B from C.");
     }else{
-        puts("No changes available from train C");
+        puts("No changes available from train C.");
         }
 
     return 0;
